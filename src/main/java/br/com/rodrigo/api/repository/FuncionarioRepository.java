b1fa294180f7929359b160d9939e011a3ebdaa2a
Rodrigo Aguiar Silva <86214343+RodrigoAguiarS@users.noreply.github.com>
2023-11-26 20:13:36 -0300

    correção e criacao das funcionalidade de deletar cargo e ajuste na forma de editar um cargo e criar.

--- src/main/java/br/com/rodrigo/api/repository/FuncionarioRepository.java
+++ src/main/java/br/com/rodrigo/api/repository/FuncionarioRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 
+    boolean existsByCargoId(Long idCargo);
 }
